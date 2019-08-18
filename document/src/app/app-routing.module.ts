d7997458d87a33149867f8b247c947949a677241
Rafael de biase <rafaelvitaldebiase@gmail.com>
2019-08-18 19:49:38 -0300

    cria rotas

--- document/src/app/app-routing.module.ts
+++ document/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
