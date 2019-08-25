3265c8eed1666b29e688cfdb19c7677a061df69c
Rafael de biase <rafaelvitaldebiase@gmail.com>
2019-08-24 23:44:27 -0300

    refactory

--- document/src/app/search/search.component.ts
+++ document/src/app/search/search.component.ts
@@ -4,7 +4,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatTableDataSource, MatSnackBar } from '@angular/material';
 import { IDocument } from '../intefaces/documentInterface';
 import { AppService } from '../app.service';
+import { DataSource } from '@angular/cdk/collections';
 import { Category } from '../enums/categoryEnum';
+import { tap } from 'rxjs/operators';
+import { IPatch } from '../intefaces/patchInterface';
+import { Observable } from 'rxjs';
+import { DocumentDataSource } from '../DocumentDataSource';
 
 @Component({
   selector: 'do-search',
@@ -12,23 +17,18 @@ import { Category } from '../enums/categoryEnum';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  
-  private data: IDocument[] = [];
-  public fileUrl;
-  public displayedColumns: string[] = ['code', 'title', 'process', 'category', 'file', 'action'];
-  public dataSource = new MatTableDataSource<IDocument>(this.data);
-  
+
   public formGroup: FormGroup;
-  public formGroupEdit: FormGroup;
-  public edit = false;
-  public save = true;
-  
+
+  private dataSource: DocumentDataSource;
+
   constructor(private sanitizer: DomSanitizer, private builder: FormBuilder,
-    private snack: MatSnackBar, private service: AppService) { }
-  
-    ngOnInit() {
+              private snack: MatSnackBar, private service: AppService) { }
+
+  ngOnInit() {
+    this.dataSource = new DocumentDataSource(this.service);
     this._form();
-    this._formEdit();
+
   }
 
   private _form(): void {
@@ -37,109 +37,15 @@ export class SearchComponent implements OnInit {
     });
   }
 
-  private _formEdit(): void {
-    this.formGroupEdit = this.builder.group({
-      code: [null],
-      title: [null],
-      process: [null],
-      category: [null]
-    });
-  }
-
   private isNumeric(str: string): boolean {
-    var numberTest = /^[0-9]+$/;
-    return numberTest.test(str)
+      const numberTest = /^[0-9]+$/;
+      return numberTest.test(str);
   }
 
-  public getByCodeClick() {
-    const code: string = this.formGroup.get('search').value;
-
-    if (this.isNumeric(code)) {
-      const relativeUri = `document/getbycode/${code}`;
-      this.service.get(relativeUri).subscribe(response => {
-        this.data.push(response);
-      });
-    }
-    else {
-      this.snack.open('Para executar esta pequisa favor informar o código, deve conter apenas números',
-        'Pesquisa pelo código', { duration: 3000 });
-    }
-
-  }
-
-  public getByTitleClick() {
-    const title: string = this.formGroup.get('search').value;
-
-    if (title !== null) {
-      const relativeUri = `document/getbytitle/${title}`;
-      this.service.get(relativeUri).subscribe(response => {
-        this.data.push(response);
-      });
-    }
-    else {
-      this.snack.open('Favor digitar o título a ser pesquisado',
-        'Pesquisa pelo titulo', { duration: 3000 });
-    }
-  }
-
-  public getByProcessClick() {
-    const process: string = this.formGroup.get('search').value;
-
-    if (process !== null) {
-      const relativeUri = `document/getbyprocess/${process}`;
-      this.service.get(relativeUri).subscribe(response => {
-        this.data.push(response);
-      });
-    }
-    else {
-      this.snack.open('Favor digitar o processo a ser pesquisado',
-        'Pesquisa pelo processo', { duration: 3000 });
-    }
-  }
-
-  public getByCategoryClick() {
-    const rn1Value = Category.RN1;
-    const rn2Value = Category.RN2;
-    const rn3Value = Category.RN3;
-    const rn4Value = Category.RN4;
-    const rn5Value = Category.RN5;
-
-    const category: string = this.formGroup.get('search').value;
-
-    if (category !== null) {
-      if (category === Category[rn1Value] || category === Category[rn2Value] ||
-        category === Category[rn3Value] || category === Category[rn4Value] ||
-        category === Category[rn5Value]) {
-        const relativeUri = `document/getbycategory/${category}`;
-        this.service.get(relativeUri).subscribe(response => {
-          this.data.push(response);
-        });
-      }
-      else {
-        this.snack.open('Favor digitar o nome da categoria correto ou realizar a pesquisa correta',
-          'Pesquisa pelo processo', { duration: 3000 });
-      }
-    }
-    else {
-      this.snack.open('Favor digitar o processo a ser pesquisado',
-        'Pesquisa pelo processo', { duration: 3000 });
-    }
-  }
-
-  public downloadCick(key: number) {
-    this.service.getFile(key).subscribe( response => {
-      const blob = new Blob([response.blob], { type: response.contentType });
-      this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
-    });
-  }
-
-  public editClick() {
-    this.edit = true;
-    this.save = false;
-  }
-
-  public saveClick(row: any) {
-    console.log(row);
+  public codeClick() {
+    const code = this.formGroup.get('search').value;
+    this.dataSource.CodeSearch(code);
   }
 
 }
+
